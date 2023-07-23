8e53d107db97e64b9c5e5853d6d9d4aa29d100b7
EmilianoMarcle <89201123+EmilianoMarcle@users.noreply.github.com>
2023-07-23 03:14:39 -0300

    Update and rename EJEMPLO_UART_TX_Interrupt.c to Ejemplo_UART_TX_Interrupt.c

--- UART/Ejemplo_UART_TX_Interrupt.c
+++ UART/Ejemplo_UART_TX_Interrupt.c
@@ -3,7 +3,7 @@
  Name        : Ejemplo_UART_TX_Int.c
  Author      : Marclé Emiliano
  Version     : 1.0
- Description :
+ Description :  Transmisión UART2 usando interrupción por THRE. 
 ===============================================================================
 */
 
