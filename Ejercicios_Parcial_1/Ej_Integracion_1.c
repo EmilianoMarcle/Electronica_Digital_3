ee976dc1fb284f09353addc49fee2cc58b44c6f8
EmilianoMarcle <89201123+EmilianoMarcle@users.noreply.github.com>
2023-09-09 02:04:12 -0300

    Add files via upload

--- Ejercicios_Parcial_1/Ej_Integracion_1.c
+++ Ejercicios_Parcial_1/Ej_Integracion_1.c
@@ -2,26 +2,22 @@
 ===============================================================================
  Alumno		: Marclé Emiliano
  Carrera	: Ing. Electronica
-
- Descripcion : Algoritmo de antirrebote de un pulsador: Escribir un programa en C
-  	  	  	  que ante la interrupción por flanco de subida del pin P0.1, configurado como
-  	  	  	  entrada digital con pull-down interno, se incremente un contador de un dígito,
-  	  	  	  se deshabilite esta interrupción y se permita la interrupción por systick cada
-  	  	  	  20 milisegundos. En cada interrupción del systick se testeará una vez el pin P0.1.
-  	  	  	  Solo para el caso de haber testeado 3 estados altos seguidos se sacará por los
-  	  	  	  pines del puerto P2.0 al P2.7 el equivalente en ascii del valor del contador,
-  	  	  	  se desactivará las interrupción por systick y se habilitará nuevamente la
-  	  	  	  interrupción por P0.1. Por especificación de diseño se pide que los pines
-   	   	   	   del puerto 2 que no sean utilizados deben estar enmascarados por hardware.
-  	  	  	  Considerar que el CPU se encuentra funcionando con el oscilador interno RC (4Mhz).
+ Descripcion : Algoritmo de antirrebote de un pulsador: Escribir un programa en C que ante la interrupción por
+ flanco de subida del pin P0.1, configurado como entrada digital con pull-down interno, se incremente un contador de un dígito,
+ se deshabilite esta interrupción y se permita la interrupción por systick cada 20 milisegundos.
+ En cada interrupción del systick se testeará una vez el pin P0.1.
+ Solo para el caso de haber testeado 3 estados altos seguidos se sacará por los pines del puerto P2.0 al P2.7
+ el equivalente en ascii del valor del contador, se desactivará las interrupción por systick y se habilitará nuevamente la interrupción por P0.1.
+ Por especificación de diseño se pide que los pines del puerto 2 que no sean utilizados deben estar enmascarados por hardware.
+ Considerar que el CPU se encuentra funcionando con el oscilador interno RC (4Mhz).
 ===============================================================================
 */
 
 #include "LPC17xx.h"
 
 /* ------ Macros ------ */
-#define	  TICKS	  		(uint32_t)  80000	  // TICKS = RELOAD+1 = 25ms*4MHz
-//#define	  TICKS	  		(uint32_t)  2500000	  // TICKS = RELOAD+1 = 25ms*100MHz
+//#define	  TICKS	  		(uint32_t)  80000	  // TICKS = RELOAD+1 = 25ms*4MHz
+#define	  TICKS	  		(uint32_t)  2500000	  // TICKS = RELOAD+1 = 25ms*100MHz
 #define   MAX_COUNT 	(uint16_t)  3       // Cantidad maxima de interrupciones de SysTick contadas.
 
 
@@ -43,6 +39,13 @@ int main(void){
 
 	while(1){
 		if(flagDigito){
+			/* Incremento de contador de un dígito */
+			digito++;
+			/* La cuenta es de 0 a 9 */
+				if(digito > 9){
+					digito = 0;
+				}
+			/* Saca el valor por los pines P2.0 a P2.9*/
 			valor = (digito + 48);   // Convierte el digito a ASCII sumando 32 hexadecimal o 48 decimal.
 			LPC_GPIO2->FIOPIN0 = valor;		  // Saca el valor por P2.0 a P2.7
 			flagDigito = 0;
@@ -76,9 +79,10 @@ void configIntGPIO(void){
  * Función de configuración de SysTick.
  */
 void configSysTick(uint32_t ticks){
-	SysTick->LOAD = ticks-1;					// Carga valor de recarga.
-	SysTick->VAL = 0;						    // Pone contador a cero.
-	SysTick->CTRL |= (1<<0)|(1<<1)|(1<<2);		// Habilita el SysTick, la interrupción y selecciona cclk.
+	SysTick-> CTRL &= ~(3<<0);					// Desactiva el SysTick, y la interrupción.
+	SysTick-> LOAD = ticks-1;					// Carga valor de recarga.
+	SysTick-> VAL = 0;						    // Pone contador a cero.
+	SysTick-> CTRL |= (1<<0)|(1<<1)|(1<<2);		// Habilita el SysTick, la interrupción y selecciona cclk.
 	return;
 }
 
@@ -86,11 +90,6 @@ void configSysTick(uint32_t ticks){
 void EINT3_IRQHandler(void){
 	/* Verifica si la interrupción fue por P0.1 */
 	if((LPC_GPIOINT->IO0IntStatR) & (1<<1)){
-		/* Incremento de contador de un dígito */
-		digito++;
-		if(digito > 9){
-			digito = 0;
-		}
 		/* Desactivación Int. en P0.1 */
 		LPC_GPIOINT->IO0IntEnR &= ~(1<<1);		// Deshabilita int. en P0.1
 		LPC_GPIOINT->IO0IntClr |= (1<<1);		// Limpia flag de int.
@@ -104,12 +103,14 @@ void EINT3_IRQHandler(void){
 /*
  * Handler del SysTick
  * Cada 20ms se entra a la interrupción.
+ * Si se lee un 0 lógico en P0.1 antes de contar las interrupciones
+ * - Se descativa SysTick
+ * - Se configura de nuevo la int. en P0.1
+ * - No se saca el dígito por el P2.0 a P2.7
  * Si se tiene tres valores altos seguidos en P0.1:
  * - Se desactiva el SysTick
  * - Se configura de nuevo la int. en P0.1
  * - Se activa el flag para sacar el digito por P2.0 a P2.7
- * El SysTick solo de desactiva si se tienen 3 valores seguidos en P0.1
- * por lo que quedara entrando indefinidamente a el handler.
  * Hay que elegir correctamente el valor de MAX_COUNT.
  */
 void SysTick_Handler(void){
@@ -117,16 +118,14 @@ void SysTick_Handler(void){
 	SysTick->CTRL &= SysTick->CTRL;				// Limpia flag int.
 	if(LPC_GPIO0->FIOPIN & (1<<1)){
 		intCount++;
-		if(intCount == MAX_COUNT){
-			SysTick->CTRL = 0;					// Desactiva el SysTick.
-			configIntGPIO();					// Configura Int. en P0.1
-			flagDigito = 1;                     // Activa el flag para sacar el digito por puerto.
-			intCount = 0;						// Pone el contador de interrupciones a 0.
+		if(intCount < MAX_COUNT){
+			return;
 		}
+		else flagDigito = 1;     // Activa el flag para sacar el digito por puerto.
 	}
-	else{
-		intCount = 0;
-	}
+	SysTick-> CTRL &= ~(3<<0);			// Desactiva el SysTick, y la interrupción.	// Desactiva el SysTick.
+	configIntGPIO();					// Configura Int. en P0.1
+	intCount = 0;						// Pone el contador de interrupciones a 0.
 	return;
 }
 
